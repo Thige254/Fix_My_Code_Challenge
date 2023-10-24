313d8e99fbdcbb39b41c4afa901504f22120a277
Thige254 <thigetj@gmail.com>
2023-10-24 10:57:56 +0300

    corrected the betty errors on main.c fiule

--- 0x00-challenge/4-delete_dnodeint/main.c
+++ 0x00-challenge/4-delete_dnodeint/main.c
@@ -4,66 +4,54 @@
 #include "lists.h"
 
 /**
+ * initialize_list - initialize the list with nodes
+ * @head: double pointer to the head of the list
+ */
+void initialize_list(dlistint_t **head)
+{
+	add_dnodeint_end(head, 0);
+	add_dnodeint_end(head, 1);
+	add_dnodeint_end(head, 2);
+	add_dnodeint_end(head, 3);
+	add_dnodeint_end(head, 4);
+	add_dnodeint_end(head, 98);
+	add_dnodeint_end(head, 402);
+	add_dnodeint_end(head, 1024);
+	print_dlistint(*head);
+	printf("-----------------\n");
+}
+
+/**
+ * perform_deletions - delete nodes from the list
+ * @head: double pointer to the head of the list
+ */
+void perform_deletions(dlistint_t **head)
+{
+	int i;
+
+	delete_dnodeint_at_index(head, 5);
+	print_dlistint(*head);
+	printf("-----------------\n");
+
+	for (i = 0; i < 14; i++)
+	{
+		delete_dnodeint_at_index(head, 0);
+		print_dlistint(*head);
+		printf("-----------------\n");
+	}
+}
+
+/**
  * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-	dlistint_t *head;
+	dlistint_t *head = NULL;
 
-	head = NULL;
-	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);
-	add_dnodeint_end(&head, 2);
-	add_dnodeint_end(&head, 3);
-	add_dnodeint_end(&head, 4);
-	add_dnodeint_end(&head, 98);
-	add_dnodeint_end(&head, 402);
-	add_dnodeint_end(&head, 1024);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 5);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	printf("-----------------\n");
-	delete_dnodeint_at_index(&head, 0);
-	print_dlistint(head);
+	initialize_list(&head);
+	perform_deletions(&head);
 	free_dlistint(head);
 	return (0);
 }

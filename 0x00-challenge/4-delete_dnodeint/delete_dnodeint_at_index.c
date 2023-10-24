040a920c55759b892058a21653f1c09978d0b988
Thige254 <thigetj@gmail.com>
2023-10-24 10:49:24 +0300

    changed indenytation for delete_dlintst...

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,31 +11,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    unsigned int i;
-    dlistint_t *current, *next_node, *prev_node;
+	unsigned int i;
+	dlistint_t *current, *next_node, *prev_node;
 
-    if (!head || !*head) /* Check if head is NULL */
-        return (-1);
+	if (!head || !*head) /* Check if head is NULL */
+		return (-1);
 
-    current = *head;
-    for (i = 0; current && i < index; i++) /* Move to the index you need */
-        current = current->next;
+	current = *head;
+	for (i = 0; current && i < index; i++) /* Move to the index you need */
+		current = current->next;
 
-    if (!current)
-        return (-1);
+	if (!current)
+		return (-1);
 
-    next_node = current->next;
-    prev_node = current->prev;
+	next_node = current->next;
+	prev_node = current->prev;
 
-    if (prev_node) /* If not the first node */
-        prev_node->next = next_node;
-    else /* If it's the first node, adjust head */
-        *head = next_node;
+	if (prev_node) /* If not the first node */
+		prev_node->next = next_node;
+	else /* If it's the first node, adjust head */
+		*head = next_node;
 
-    if (next_node) /* If not the last node */
-        next_node->prev = prev_node;
+	if (next_node) /* If not the last node */
+		next_node->prev = prev_node;
 
-    free(current); /* Free the current node */
+	free(current); /* Free the current node */
 
-    return (1);
+	return (1);
 }
